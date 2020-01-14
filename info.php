c216e299d4879129e8ad3c3ec23f7271323a9756
huyIttos2 <huyittos2071195@gmail.com>
2020-01-13 22:38:13 -0800

    Changed return type of my string len to size_t

---
+++ info.php
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+
+int my_strlen(char *s)
+{
+   char *p = s;
+
+   while (*p)
+      ++p;
+
+   return (p - s);
+}
++size_t my_strlen(char *s)
+{
+   char *p = s;
+   @@ -18,7 +18,7 @@ int main(void)
+};
+for (i = 0; i < 2; ++i)
+{
+   - printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+   + printf("string lenght of %s = %lu\n", s[i], my_strlen(s[i]));
+   return 0;
+}
+int main(void)
+{
+   int i;
+   char *s[] = 
+   {
+      "Git tutorials",
+      "Tutorials Point"
+   };
+
+   for (i = 0; i < 2; ++i)
+      
+   printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+
+   return 0;
+}
