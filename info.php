7d6596a6686ad9112d3e69185498d75516ffb885
huyIttos2 <huyittos2071195@gmail.com>
2020-01-13 23:03:45 -0800

    Added my_strcpy function

--- info.php
+++ info.php
@@ -23,6 +23,20 @@ int main(void)
    for (i = 0; i < 2; ++i)
       
    printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
-
+    
+   int i; 
+   +
+   char p1[32];
+   char *s[] = 
+   {
+      "Git tutorials",
+      "Tutorials Point"
+      @@ -20,5 +31,7 @@ int main(void)
+      for (i = 0; i < 2; ++i)
+      printf("string lenght of %s = %lu\n", s[i], my_strlen(s[i]));
+      +
+      printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
+      +
+      return 0;
    return 0;
 }
